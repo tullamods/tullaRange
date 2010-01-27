1b5362c547bafd2c75e2247d4070be269aedec53
= <=>
2010-01-27 18:52:32 -0500

    moving tullaRange from svn...

---
+++ tullaRange_Config/localization/localization.lua
@@ -0,0 +1,14 @@
+--[[
+	tullaRangeConfig localization
+--]]
+
+TULLARANGE_CONFIG_LOCALS = TULLARANGE_CONFIG_LOCALS or {}
+local L = TULLARANGE_CONFIG_LOCALS
+if not L then 
+	return 
+end
+
+L.ColorSettings = 'Colors'
+L.ColorSettingsTitle = 'tullaRange color configuration settings.'
+L.Color_oor = 'Out of Range Color'
+L.Color_oom = 'Out of Mana Color'
---
+++ tullaRange_Config/panels/default.lua
@@ -0,0 +1,9 @@
+local DEFAULT_PANEL = 'ColorOptions'
+
+--a hack panel, this is designed to force open to the general options panel when clicked
+local tullaRangeOptions = tullaRangeConfig.OptionsPanel:New('tullaRange', nil, 'tullaRange')
+tullaRangeOptions:SetScript('OnShow', function(self)
+	local panel = tullaRangeConfig[DEFAULT_PANEL]
+	InterfaceOptionsFrame_OpenToCategory(panel)
+	self:Hide()
+end)
