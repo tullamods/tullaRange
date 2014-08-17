df1c0df6b2fa91928c63bc11f18ddd47e47fc729
Jason Greer <tuller@tullamods.com>
2014-08-17 16:54:21 -0400

    Many bugfixes

--- tullaRange.lua
+++ tullaRange.lua
@@ -7,6 +7,8 @@
 --locals and speed
 local AddonName, Addon = ...
 local _G = _G
+local next = next
+local pairs = pairs
 
 local DB_KEY = 'TULLARANGE_COLORS'
 local UPDATE_DELAY = 0.15
@@ -18,10 +20,12 @@ local IsActionInRange = _G['IsActionInRange']
 local IsUsableAction = _G['IsUsableAction']
 local HasAction = _G['HasAction']
 local GetTime = _G['GetTime']
-local Timer_After = _G['CTimer'].After
+local Timer_After = _G['C_Timer'].After
 
+--[[
+	Helper Functions
+--]]
 
---code for handling defaults
 local function removeDefaults(tbl, defaults)
 	for k, v in pairs(defaults) do
 		if type(tbl[k]) == 'table' and type(v) == 'table' then
@@ -33,6 +37,7 @@ local function removeDefaults(tbl, defaults)
 			tbl[k] = nil
 		end
 	end
+
 	return tbl
 end
 
@@ -44,40 +49,40 @@ local function copyDefaults(tbl, defaults)
 			tbl[k] = v
 		end
 	end
+
 	return tbl
 end
 
 
---[[ The main thing ]]--
+--[[ 
+	The main thing 
+--]]
 
 function Addon:Load()
+	self.buttonColors = {}
 	self.buttonsToUpdate = {}
-
-	self.started = nil
-
-	self.updateFinshed = function()
-		local elapsed = GetTime() - self.started
-		self.started = nil
-
-		self:Update()
-	end
 
 	local f = CreateFrame('Frame', nil, _G['InterfaceOptionsFrame'])
 
 	f:SetScript('OnShow', function()
 		f:SetScript('OnShow', nil)
-
 		LoadAddOn(AddonName .. '_Config')
 	end)
 
-	f:SetScript('OnEvent', self.OnEvent)
+	f:SetScript('OnEvent', function(frame, ...)
+		self:OnEvent(...)
+	end)
 
 	f:RegisterEvent('PLAYER_LOGIN')
 	f:RegisterEvent('PLAYER_LOGOUT')
+
+	_G[AddonName] = self
 end
 
 
---[[ Frame Events ]]--
+--[[ 
+	Frame Events 
+--]]
 
 function Addon:OnEvent(event, ...)
 	local action = self[event]
@@ -86,8 +91,6 @@ function Addon:OnEvent(event, ...)
 		action(self, event, ...)
 	end
 end
-
---[[ Game Events ]]--
 
 function Addon:PLAYER_LOGIN()
 	self:SetupDatabase()
@@ -99,15 +102,52 @@ function Addon:PLAYER_LOGOUT()
 end
 
 
---[[ Actions ]]--
+--[[ 
+	Button Hooking 
+--]]
+
+do
+	local function button_UpdateStatus(button)
+		Addon:UpdateButtonStatus(button)
+	end
+
+	local function button_UpdateUsable(button)
+		Addon:UpdateButtonUsable(button, true)
+	end
+
+	local function button_Register(button)
+		Addon:Register(button)
+	end
+
+	function Addon:HookActionEvents()
+		hooksecurefunc('ActionButton_OnUpdate', button_Register)
+		hooksecurefunc('ActionButton_UpdateUsable', button_UpdateUsable)
+		hooksecurefunc('ActionButton_Update', button_UpdateStatus)
+	end
+
+	function Addon:Register(button)
+		button:HookScript('OnShow', button_UpdateStatus)
+		button:HookScript('OnHide', button_UpdateStatus)
+		button:SetScript('OnUpdate', nil)
+
+		self:UpdateButtonStatus(button)
+	end
+end
+
+
+--[[ 
+	Actions 
+--]]
 
 function Addon:RequestUpdate()
+	if not next(self.buttonsToUpdate) then return end
+
 	if not self.timerDone then
 		self.timerDone = function()
 			local elapsed = GetTime() - self.started
 			self.started = nil
 
-			if self:Update(elapsed) then
+			if self:UpdateButtons(elapsed) then
 				self:RequestUpdate()
 			end
 		end
@@ -120,142 +160,102 @@ function Addon:RequestUpdate()
 	end
 end
 
-function Addon:Update(elapsed)
-	return self:UpdateButtons(elapsed)
-end
-
-function Addon:UpdateActive()
-	if next(self.buttonsToUpdate) then
-		self:RequestUpdate()
-	end
-end
-
 function Addon:UpdateButtons(elapsed)
-	if next(self.buttonsToUpdate) then
-		for button in pairs(self.buttonsToUpdate) do
-			self:UpdateButton(button, elapsed)
-		end
-
-		return true
+	if not next(self.buttonsToUpdate) then 
+		return false 
 	end
 
-	return false
+	for button in pairs(self.buttonsToUpdate) do
+		self:UpdateButton(button, elapsed)
+	end
+
+	return true
 end
 
 function Addon:UpdateButton(button, elapsed)
 	self:UpdateButtonUsable(button)
-	self:UpdateFlash(button, elapsed)
-end
-
-function Addon:UpdateButtonStatus(button)
-	local action = button.action
-
-	if button:IsVisible() and action and HasAction(action) and ActionHasRange(action) then
-		self.buttonsToUpdate[button] = true
-	else
-		self.buttonsToUpdate[button] = nil
-	end
-
-	self:UpdateActive()
-end
-
-
-
---[[ Button Hooking ]]--
-
-do
-	local function button_OnShow(button)
-		Addon:UpdateButtonStatus(button)
-	end
-
-	local function button_OnHide(button)
-		Addon:UpdateButtonStatus(button)
-	end
-
-	local function button_OnUpdateUsable(button)
-		Addon:UpdateButtonUsable(button, true)
-	end
-
-	local function button_OnUpdate(button)
-		Addon:Register(button)
-	end
-
-	function Addon:HookActionEvents()
-		hooksecurefunc('ActionButton_OnUpdate', button_OnUpdate)
-		hooksecurefunc('ActionButton_UpdateUsable', button_OnUpdateUsable)
-		hooksecurefunc('ActionButton_Update', button_OnUpdate)
-	end
-
-	function Addon:Register(button)
-		button:HookScript('OnShow', button_OnShow)
-		button:HookScript('OnHide', button_OnHide)
-		button:SetScript('OnUpdate', nil)
-
-		self:UpdateButtonStatus(button)
-	end
-end
-
---[[ Button Updates ]]--
-
-function Addon:SetButtonColor(button, colorType)
-	if button.tullaRangeColor ~= colorType then
-		button.tullaRangeColor = colorType
-
-		local r, g, b = self:GetColor(colorType)
-		button.icon:SetVertexColor(r, g, b)
-	end
+	self:UpdateButtonFlash(button, elapsed)
 end
 
 function Addon:UpdateButtonUsable(button, force)
 	if force then
-		button.tullaRangeColor = nil
+		self.buttonColors[button] = nil
 	end
 
 	local action = button.action
 	local isUsable, notEnoughMana = IsUsableAction(action)
 
-	--usable
+	--usable (ignoring target information)
 	if isUsable then
+		local inRange = IsActionInRange(action)
+
 		--but out of range
-		if IsActionInRange(action) == 0 then
+		if inRange == false then
 			self:SetButtonColor(button, 'oor')
-		else
+		elseif inRange == true then
 			self:SetButtonColor(button, 'normal')
+		-- else
+		-- 	self:SetButtonColor(button, 'unusable')
 		end
 	--out of mana
 	elseif notEnoughMana then
 		self:SetButtonColor(button, 'oom')
 	--unusable
 	else
-		button.tullaRangeColor = 'unusuable'
+		self:SetButtonColor(button, 'unusable')
 	end
 end
 
-function Addon:UpdateFlash(button, elapsed)
-	if button.flashing then
-		local flashtime = button.flashtime - elapsed
+function Addon:UpdateButtonFlash(button, elapsed)
+	if button.flashing ~= 1 then return end
 
-		if flashtime <= 0 then
-			local overtime = -flashtime
-			if overtime >= ATTACK_BUTTON_FLASH_TIME then
-				overtime = 0
-			end
-			flashtime = ATTACK_BUTTON_FLASH_TIME - overtime
+	local flashtime = button.flashtime - elapsed
 
-			local flashTexture = button.Flash
-			if flashTexture:IsShown() then
-				flashTexture:Hide()
-			else
-				flashTexture:Show()
-			end
+	if flashtime <= 0 then
+		local overtime = -flashtime
+
+		if overtime >= ATTACK_BUTTON_FLASH_TIME then
+			overtime = 0
 		end
 
-		button.flashtime = flashtime
+		flashtime = ATTACK_BUTTON_FLASH_TIME - overtime
+
+		local flashTexture = button.Flash
+		if flashTexture:IsShown() then
+			flashTexture:Hide()
+		else
+			flashTexture:Show()
+		end
 	end
+
+	button.flashtime = flashtime
+end
+
+function Addon:UpdateButtonStatus(button)
+	local action = button.action
+
+	if action and button:IsVisible() and HasAction(action) then
+		self.buttonsToUpdate[button] = true
+	else
+		self.buttonsToUpdate[button] = nil
+	end
+
+	self:RequestUpdate()
+end
+
+function Addon:SetButtonColor(button, colorIndex)
+	if self.buttonColors[button] == colorIndex then return end
+
+	self.buttonColors[button] = colorIndex
+
+	local r, g, b = self:GetColor(colorIndex)
+	button.icon:SetVertexColor(r, g, b)
 end
 
 
---[[ Configuration ]]--
+--[[ 
+	Configuration 
+--]]
 
 function Addon:SetupDatabase()
 	local sets = _G[DB_KEY]
@@ -269,7 +269,7 @@ function Addon:SetupDatabase()
 end
 
 function Addon:CleanupDatabase()
-	local sets = _G[DB_KEY]
+	local sets = self.sets
 
 	if sets then
 		removeDefaults(sets, self:GetDatabaseDefaults())
@@ -280,7 +280,8 @@ function Addon:GetDatabaseDefaults()
 	return {
 		normal = {1, 1, 1},
 		oor = {1, 0.3, 0.1},
-		oom = {0.1, 0.3, 1}
+		oom = {0.1, 0.3, 1},
+		unusable = {0.4, 0.4, 0.4}
 	}
 end
 
@@ -315,8 +316,4 @@ end
 
 
 --[[ Load The Thing ]]--
-
-do
-	Addon:Load()
-	_G[AddonName] = Addon
-end
+Addon:Load()
