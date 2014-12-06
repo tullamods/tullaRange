1abfe851d5bfec8f38bb190af05e9409e847e09f
Kuzunkhaa <spata.marco@gmail.com>
2014-12-06 12:49:55 +0100

    itIT Localization

---
+++ localization/localization.it.lua
@@ -0,0 +1,22 @@
+--[[tullaRange Config Localization - Italian]]
+
+if GetLocale() ~= 'itIT' then return end
+
+local AddonName, Addon = ...
+local L = Addon.L
+
+L.ColorSettings = 'Colori'
+
+L.ColorSettingsTitle = 'Impostazioni per la configurazione del colore'
+
+L.oor = 'Bersaglio distante'
+
+L.oom = 'Mana scarso'
+
+L.unusable = 'Non utilizzabile'
+
+L.Red = 'Rosso'
+
+L.Green = 'Verde'
+
+L.Blue = 'Blu'
