845655d7a0fddc88ac6983ecb9fd6292956a7d92
Jason Greer <tuller@tullamods.com>
2019-06-24 21:06:01 -0400

    merged in config

---
+++ tullaRange_Config/localization/localization.de.lua
@@ -0,0 +1,22 @@
+--[[tullaRange Config Localization - German]]
+
+if GetLocale() ~= 'deDE' then return end
+
+local AddonName, Addon = ...
+local L = Addon.L
+
+L.ColorSettings = 'Farben'
+
+L.ColorSettingsTitle = 'Hier kannst du Farbeinstellungen vornehmen'
+
+L.oor = 'Außer Reichweite'
+
+L.oom = 'Nicht genug Mana'
+
+L.unusable = 'Nicht benutzbar'
+
+L.Red = 'Rot'
+
+L.Green = 'Grün'
+
+L.Blue = 'Blau'
